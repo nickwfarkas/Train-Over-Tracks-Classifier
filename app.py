2e9c96d9ccf89e19a13eb12fad13d5fe7b4ca2e0
Nicholas Farkas <nicholaswfarkas@gmail.com>
2023-01-10 22:50:48 -0500

    test

--- app.py
+++ app.py
@@ -10,20 +10,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    img = get_current_crossing_image()
-    img = transform_to_1D(img)
-    model = load_model()
-    prediction = (str(model.predict([img])))
-    return prediction
+    return "hello"
+#     img = get_current_crossing_image()
+#     img = transform_to_1D(img)
+#     model = load_model()
+#     prediction = (str(model.predict([img])))
+#     return prediction
 
-def get_current_crossing_image():
-    image_request = requests.get("http://rrcrossings.woodhavenmi.org/allen.jpg?rnd=")
-    image_bytes = BytesIO(image_request.content)
-    return Image.open(image_bytes)
+# def get_current_crossing_image():
+#     image_request = requests.get("http://rrcrossings.woodhavenmi.org/allen.jpg?rnd=")
+#     image_bytes = BytesIO(image_request.content)
+#     return Image.open(image_bytes)
 
-def transform_to_1D(img: Image):
-    return np.asarray(img).ravel()
+# def transform_to_1D(img: Image):
+#     return np.asarray(img).ravel()
 
-def load_model() -> Pipeline:
-    with open("./src/model/tot_model", 'rb') as pickle_file:
-        return pickle.load(pickle_file)
+# def load_model() -> Pipeline:
+#     with open("./src/model/tot_model", 'rb') as pickle_file:
+#         return pickle.load(pickle_file)
