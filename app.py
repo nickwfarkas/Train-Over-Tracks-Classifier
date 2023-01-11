46d2245954296fb3f22a322cc1b0bfe21d44f04b
Nicholas Farkas <nicholaswfarkas@gmail.com>
2023-01-11 16:34:53 -0500

    Testing

--- app.py
+++ app.py
@@ -5,28 +5,30 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 from sklearn.pipeline import Pipeline
+from joblib import dump, load
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-     img = get_current_crossing_image()
-#     img = transform_to_1D(img)
-     model = load_model()
-     prediction = (str(model.predict([img])))
-     return prediction
+    # img = get_current_crossing_image()
+    # img = transform_to_1D(img)
+    # model = load_model()
+    # prediction = (str(model.predict([img])))
+    f = open("hello.txt", "r")
+    return f.read()
 
 def get_current_crossing_image():
     image_request = requests.get("http://rrcrossings.woodhavenmi.org/allen.jpg?rnd=")
     image_bytes = BytesIO(image_request.content)
     return Image.open(image_bytes)
 
-# def transform_to_1D(img: Image):
-#     return np.asarray(img).ravel()
+def transform_to_1D(img: Image):
+    return np.asarray(img).ravel()
 
 def load_model():
-    with open("./src/model/tot_model", 'rb') as pickle_file:
-        return pickle.load(pickle_file)
+    with open("./src/model/model.joblib", 'rb') as model_file:
+        return load(model_file)
 
 # def load_model():
 #     with open("./src/model/tot_model", 'rb') as pickle_file:
