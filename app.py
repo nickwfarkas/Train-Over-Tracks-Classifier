a9125d0f63b0fb8e7f837aafe8ab69d596c6016d
Nicholas Farkas <nicholaswfarkas@gmail.com>
2023-01-15 21:03:28 -0500

    Added status codes

--- app.py
+++ app.py
@@ -19,10 +19,18 @@ def home():
 
     model = load_model()
     try:
-        prediction = (str(model.predict([img])))
+        prediction = (str(model.predict([img])[0]))
     except:
-        return ("Failed to predict")
-    return prediction
+        return str({
+            "Status": 500,
+            "Message": "Failed to Predict",
+            "Prediction": []
+        })
+    return str({
+        "Status": 200,
+        "Message": "Success",
+        "Prediction": prediction
+    })
 
 def get_current_crossing_image():
     image_request = requests.get("http://rrcrossings.woodhavenmi.org/allen.jpg?rnd=")
@@ -33,21 +41,6 @@ def transform_to_1D(img: Image):
     img = img.convert("L")
     return np.asarray(img).ravel()
 
-# def reduce_image(img: Image):
-#     img_arr = transform_to_1D(img)
-#     # with open("./src/model/transform.joblib", 'rb') as pca_file:
-#     #     pca = load(pca_file)
-#     pca = PCA(n_components=60)
-#     return pca.fit_transform(img_arr)
-
-# def load_model():
-#     with open("./model/model.joblib", 'rb') as model_file:
-#         return load(model_file)
-
 def load_model():
     with open("./src/model/tot_model", 'rb') as model_file:
         return pickle.load(model_file)
-    
-# def load_model():
-#     with open("./src/model/tot_model", 'rb') as pickle_file:
-#         return pickle.load(pickle_file)
